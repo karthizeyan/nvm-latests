50a22f4e99717f72928efd1a1cd52182eda72172
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-03-12 18:40:28 +0700

    Added verification to the latest script

--- test/specs.js
+++ test/specs.js
@@ -4,9 +4,10 @@ var request = require('supertest').agent(app.listen());
 
 describe('Homepage', function(){
 
-	it('shows up nicely without errors', function(done){
+	it('serves up a nice html page without errors', function(done){
 		request
 			.get('/')
+			.expect('Content-Type', /html/)
 			.expect(200, done);
 	});
 
@@ -15,6 +16,15 @@ describe('Homepage', function(){
 			.get('/')
 			.expect(function(res){
 				res.text.should.containEql("nvm-latests");
+			})
+			.end(done);
+	});
+
+	it('has a link to the latests script', function(done){
+		request
+			.get('/')
+			.expect(function(res){
+				res.text.should.containEql("href='/script'");
 			})
 			.end(done);
 	});
